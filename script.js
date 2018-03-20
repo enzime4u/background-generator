8388a6569d3cb0e9cdcadd8b649805df66b1462c
enzime4u <enzime4u@yahoo.com>
2018-03-20 18:16:22 +0200

    1. Write code so that the colour inputs match the background generated on the first page load.
    2. Display the initial CSS linear gradient property on page load.
    
    3. BONUS: Add a random button which generates two random numbers for the colour inputs.
    
    == solved ==

--- script.js
+++ script.js
@@ -2,12 +2,29 @@ var css = document.querySelector("h3");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
+var generateButton = document.querySelector(".generate");
 
 function setGradient() {
 	body.style.background = "linear-gradient( to right, " + color1.value + ", " + color2.value + ")";
 	css.textContent = body.style.background + ";";
 }
 
-color1.addEventListener("input", setGradient);
-color2.addEventListener("input", setGradient);
+// 2. Display the initial CSS linear gradient property on page load.
+function onLoad() {
+	setGradient();
+}
 
+function generateGradient() {
+	var randomColor1 = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+	var randomColor2 = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+	color1.value  = randomColor1;
+	color2.value = randomColor2;
+}
+
+window.onload = onLoad;
+
+// 3. BONUS: Add a random button which generates two random numbers for the colour inputs.
+generateButton.addEventListener("click", function(){
+	generateGradient();
+	setGradient();
+})
